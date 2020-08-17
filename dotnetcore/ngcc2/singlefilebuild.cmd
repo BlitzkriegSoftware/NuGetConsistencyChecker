1c708c1079c7b56fe5d07676eeb8f36571554883
Stuart Williams <spookdejur@hotmail.com>
2020-08-17 14:00:42 -0700

    Updated Documentation

--- dotnetcore/ngcc2/singlefilebuild.cmd
+++ dotnetcore/ngcc2/singlefilebuild.cmd
@@ -1,2 +1,6 @@
 @REM Single File Build
 dotnet publish -r win-x64 -c Release --self-contained
+TARGET=.\bin\Release\netcoreapp3.1\win-x64\publish
+cp ..\README.md  %TARGET%\ngcc2-README.md
+cp .\ngcc2-urls.txt %TARGET%
+dir /b %TARGET%
--- dotnetcore/ngcc2/singlefilebuild.sh
+++ dotnetcore/ngcc2/singlefilebuild.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# C:\code\blitz\NuGetConsistencyChecker\dotnetcore\ngcc2\singlefilebuild.sh
 dotnet publish -r win-x64 -c Release --self-contained
+TARGET=./bin/Release/netcoreapp3.1/win-x64/publish
+cp ../README.md  ${TARGET}/ngcc2-README.md
+cp ./ngcc2-urls.txt ${TARGET}/
+ls ${TARGET}
